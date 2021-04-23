68757bb720ab860bf2223a0808e6ef1fc5754f85
DoggyLicious <dogrgdloveryt11@gmail.com>
2021-04-23 19:47:41 -0400

    use api to fetch results

--- app.js
+++ app.js
@@ -7,6 +7,20 @@ const app = Vue.createApp({
             gender: 'male',
             picture: 'https://randomuser.me/api/portraits/men/10.jpg'
         }
+    },
+    methods: {
+        async getUser() {
+            // Fetch the random user api
+            const res = await fetch('https://randomuser.me/api')
+            const { results } = await res.json()
+
+            // Return the objects
+            this.firstName = results[0].name.first,
+            this.lastName = results[0].name.last,
+            this.email = results[0].email,
+            this.gender = results[0].gender,
+            this.picture = results[0].picture.large
+        }
     }
 })
 
